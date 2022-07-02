5bcb33365e8870a7a4dda2e60614bfb9ccc7f8a4
whs <hswongac@gmail.com>
2022-07-02 18:28:16 -0400

    Pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,7 +1,8 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  nativeBuildInputs = [
-    pkgs.ansible
-    pkgs.packer
-  ];
-}
+{
+  pkgs ? import sources.nixpkgs { },
+  sources ? import nix/sources.nix,
+}:
+pkgs.mkShell { nativeBuildInputs = [
+  pkgs.ansible
+  pkgs.packer
+]; }
